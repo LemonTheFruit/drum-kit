9cb87a45af5f99b0d7ab20512f92d6b0aa096af6
Devin M <devin.murnan@gmail.com>
2021-08-27 15:01:14 -0500

    Refactor
    refactors to the site, changes styles

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // set the var to be equal to the number of .drum selectors (classes) found in the HTML document.
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 // this for loop adds an event listener for every selector with the .drum class. It listens for clicks on the .drum class objects (buttons) and upon a click, performs an action.
 
 //detects button press
-for (var i = 0; i < numberOfDrumButtons; i++) {
+for (let i = 0; i < numberOfDrumButtons; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-    var buttonInnerHTML = this.innerHTML;
+    const buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
 
@@ -19,7 +19,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 }
 
 // detects keyboard press
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keypress", function (event) {
 
   makeSound(event.key);
 
@@ -31,47 +31,47 @@ function makeSound(key) {
   switch (key) {
 
     case "q":
-      var ride = new Audio('sounds/ride.mp3');
+      const ride = new Audio('sounds/ride.mp3');
       ride.play();
       break;
 
     case "w":
-      var crash = new Audio('sounds/crash.mp3'); //creates an HTML audio element.
+      const crash = new Audio('sounds/crash.mp3'); //creates an HTML audio element.
       crash.play();
       break;
 
     case "a":
-      var kick = new Audio('sounds/kick-bass.mp3');
+      const kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
 
     case "s":
-      var snare = new Audio('sounds/snare.mp3');
+      const snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
     case "d":
-      var hihat = new Audio('sounds/hi-hat.mp3');
+      const hihat = new Audio('sounds/hi-hat.mp3');
       hihat.play();
       break;
 
     case "h":
-      var tom1 = new Audio('sounds/tom-1.mp3');
+      const tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
       break;
 
     case "j":
-      var tom2 = new Audio('sounds/tom-2.mp3');
+      const tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
       break;
 
     case "k":
-      var tom3 = new Audio('sounds/tom-3.mp3');
+      const tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
 
     case "l":
-      var tom4 = new Audio('sounds/tom-4.mp3');
+      const tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
 
@@ -81,15 +81,15 @@ function makeSound(key) {
   }
 }
 
-  function buttonAnimation(currentKey) {
-//checks for the key used.
-//Because we assigned classes to our HTML elements with the intended keys used, we're going to search the DOM for those classes.
-    var activeButton = document.querySelector("." + currentKey);
+function buttonAnimation(currentKey) {
+  //checks for the key used.
+  //Because we assigned classes to our HTML elements with the intended keys used, we're going to search the DOM for those classes.
+  const activeButton = document.querySelector("." + currentKey);
 
-    activeButton.classList.add("pressed")
+  activeButton.classList.add("pressed")
 
-    setTimeout(function(){
-      activeButton.classList.remove("pressed");
-    }, 100);
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 
-  }
+}
